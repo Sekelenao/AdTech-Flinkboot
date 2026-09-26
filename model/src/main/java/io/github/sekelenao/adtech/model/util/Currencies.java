3827b37bfb02a0660339615ac058094b7985f81a
Sekelenao <Sekelenao@protonmail.com>
2026-09-26 10:00:28 +0200

    refactor(model): remove unused double and decimal methods from Currencies

--- model/src/main/java/io/github/sekelenao/adtech/model/util/Currencies.java
+++ model/src/main/java/io/github/sekelenao/adtech/model/util/Currencies.java
@@ -13,15 +13,7 @@ public final class Currencies {
     private Currencies() {}
 
     public static long toMicros(BigDecimal amount) {
-        Objects.requireNonNull(amount, "amount must not be null");
+        Objects.requireNonNull(amount);
         return amount.multiply(BigDecimal.valueOf(MICROS_PER_UNIT)).longValue();
-    }
-
-    public static long toMicros(double amount) {
-        return Math.round(amount * MICROS_PER_UNIT);
-    }
-
-    public static double toDecimal(long micros) {
-        return (double) micros / MICROS_PER_UNIT;
     }
 }
